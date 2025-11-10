35a48834276ce9e357fc85667d2a769cada586bc
Mole <157280874+MoleSof@users.noreply.github.com>
2025-11-10 09:40:02 -0700

    Update Depends_Installer_Ubuntu_Plucky_25.04.sh

--- Depends_Installer_Ubuntu_Plucky_25.04.sh
+++ Depends_Installer_Ubuntu_Plucky_25.04.sh
@@ -1,6 +1,6 @@
 sudo apt install nmcli nmap wireshark tcpdump aircrack-ng -y
 #add fix for complete even if one fails to install
-#Add error loging
+#Add error loging or force display with no quit on end
 wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
 echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/release/plucky plucky main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
 sudo apt update
